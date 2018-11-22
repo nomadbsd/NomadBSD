e11e3c2d5570c82526b753a09662a03317303d7a
Marcel <mk@freeshell.de>
2018-11-22 21:38:47 +0100

    Add brand and logo file for lua loader

---
+++ config/boot/lua/brand-nomadbsd.lua
@@ -0,0 +1,45 @@
+-- Copyright (c) 2018 Lars Engels
+-- All rights reserved.
+-- 
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions
+-- are met:
+-- 1. Redistributions of source code must retain the above copyright
+--    notice, this list of conditions and the following disclaimer.
+-- 2. Redistributions in binary form must reproduce the above copyright
+--    notice, this list of conditions and the following disclaimer in the
+--    documentation and/or other materials provided with the distribution.
+-- 
+-- THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+-- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+-- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+-- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+-- OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+-- HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+-- LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+-- OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+-- SUCH DAMAGE.
+-- 
+
+local drawer = require("drawer")
+local nomadbsd_brand = {
+" \027[34;1m _   _                           _\027[m ",
+" \027[34;1m| \\ | | ___  _ __ ___   __ _  __| |\027[m",
+" \027[34;1m|  \\| |/ _ \\| '_ ` _ \\ / _` |/ _` |\027[m",
+" \027[34;1m| |\\  | (_) | | | | | | (_| | (_| |\027[m",
+" \027[34;1m|_| \\_|\\___/|_| |_| |_|\\__,_|\\__,_|\027[m",
+"   \027[31;1m____ ____  ____\027[m",
+"  \027[31;1m| __ ) ___||  _ \\\027[m",
+"  \027[31;1m|  _ \\___ \\| | | |\027[m",
+"  \027[31;1m| |_) |__) | |_| |\027[m",
+"  \027[31;1m|____/____/|____/\027[m"
+}
+
+drawer.addBrand("nomadbsd", {
+	graphic = nomadbsd_brand,
+	shift = {x = 0, y = 0},
+})
+
+return true
