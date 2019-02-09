246a59d33258d2c6c4ac1d1fd8f6bb653a1b8ae3
Marcel <mk@freeshell.de>
2019-02-09 16:56:19 +0100

    Add Russian translation file.

---
+++ src/nomadbsd-setup-gui/locale/nomadbsd-setup-gui_ru.ts
@@ -0,0 +1,377 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>CommitPage</name>
+    <message>
+        <location filename="../src/wizard.cpp" line="766"/>
+        <source>Couldn&apos;t start backend &apos;%1&apos;: %2</source>
+        <translation>Фоновый процесс не может быть запущен %1: %2</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="790"/>
+        <source>An unexpected error occured while executing %1: %2</source>
+        <translation>Возникла неизвестная ошибка в процессе выполнения %1: %2</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="834"/>
+        <source>Press &quot;Finish&quot; to reboot</source>
+        <translation>Нажмите &quot;Завершить&quot; для перезагрузки</translation>
+    </message>
+</context>
+<context>
+    <name>GeliPage</name>
+    <message>
+        <location filename="../src/wizard.cpp" line="518"/>
+        <location filename="../src/wizard.cpp" line="538"/>
+        <source>Yes</source>
+        <translation>Да</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="518"/>
+        <location filename="../src/wizard.cpp" line="538"/>
+        <source>No</source>
+        <translation>Нет</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="519"/>
+        <source>Encrypt /home using Geli</source>
+        <translation>Зашифровать директорию /home использую Geli</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="520"/>
+        <source>Geli encrypted /home
+</source>
+        <translation>Директория /home зашифрованная с помощью Geli
+</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="521"/>
+        <source>NomadBSD allows you to protect your personal files by encrypting the /home partition using geli(8). If you don&apos;t know what geli(8) is, you should skip this page.
+
+</source>
+        <translation>NomadBSD позволяет защитить ваши личные файлы с помощью шифрования домашней директории /home использую geli(8). Если Вы не знаете для чего используется geli(8), то данную страницу лучше пропустить.
+
+</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="525"/>
+        <source>Define a password required to decrypt /home</source>
+        <translation>Выберите пароль с помощью которого можно будет расшифровать директорию /home</translation>
+    </message>
+</context>
+<context>
+    <name>KbdLayoutPage</name>
+    <message>
+        <location filename="../src/wizard.cpp" line="218"/>
+        <location filename="../src/wizard.cpp" line="249"/>
+        <source>Couldn&apos;t start backend &apos;%1&apos;: %2</source>
+        <translation>Фоновый процесс не может быть запущен %1: %2</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="354"/>
+        <source>Keyboard layout</source>
+        <translation>Раскладка клавиатуры</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="355"/>
+        <source>Keyboard variant</source>
+        <translation>Тип клавиатуры</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="356"/>
+        <source>Test your keyboard settings</source>
+        <translation>Проверьте настройки клавиатуры</translation>
+    </message>
+</context>
+<context>
+    <name>LocalePage</name>
+    <message>
+        <location filename="../src/wizard.cpp" line="138"/>
+        <source>Couldn&apos;t start backend &apos;%1&apos;: %2</source>
+        <translation>Фоновый процесс не может быть запущен %1: %2</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="162"/>
+        <source>Please select your language/locale</source>
+        <translation>Пожалуйста, выберите свой язык</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="182"/>
+        <source>Next</source>
+        <translation>Далее</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="183"/>
+        <source>Back</source>
+        <translation>Назад</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="184"/>
+        <source>Commit</source>
+        <translation>Применить</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="185"/>
+        <source>Finish</source>
+        <translation>Завершить</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="186"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordPage</name>
+    <message>
+        <location filename="../src/wizard.cpp" line="460"/>
+        <source>Set password for user and root
+</source>
+        <translation>Выберите пароль для текущего пользователя и суперпользователя
+</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="461"/>
+        <source>The password you choose here will be used for your user account (nomad), and for the administration account (root)
+
+</source>
+        <translation>Пароль, который Вы выберете, будет использоваться для текущего пользователя (nomad) и для суперпользователя (root)
+
+</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordWidget</name>
+    <message>
+        <location filename="../src/password.cpp" line="35"/>
+        <location filename="../src/password.cpp" line="88"/>
+        <source>Passwords do not match</source>
+        <translation>Пароли не совпадают</translation>
+    </message>
+    <message>
+        <location filename="../src/password.cpp" line="85"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+<context>
+    <name>ProgramsPage</name>
+    <message>
+        <location filename="../src/wizard.cpp" line="596"/>
+        <source>Couldn&apos;t start backend &apos;%1&apos;: %2</source>
+        <translation>Фоновый процесс не может быть запущен %1: %2</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="638"/>
+        <source>Shell</source>
+        <translation>Командная оболочка</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="638"/>
+        <source>Editor</source>
+        <translation>Текстовый редактор</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="638"/>
+        <source>GUI Editor</source>
+        <translation>Графический текстовый редактор</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="639"/>
+        <source>Email client</source>
+        <translation>Почтовый клиент</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="639"/>
+        <source>Web browser</source>
+        <translation>Web браузер</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="639"/>
+        <source>File manager</source>
+        <translation>Файловый менеджер</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="643"/>
+        <source>Choose your default applications
+</source>
+        <translation>Выберите программы доступные по-умолчанию
+</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="644"/>
+        <source>Please choose your preferred shell, editors, browser, and email client
+</source>
+        <translation>Пожалуйста, выберите командную оболочку, редакторы, браузер и почтовый клиент
+</translation>
+    </message>
+</context>
+<context>
+    <name>SetupWizard</name>
+    <message>
+        <location filename="../src/wizard.cpp" line="64"/>
+        <source>NomadBSD Setup</source>
+        <translation>Мастер настройки NomadBSD</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.h" line="57"/>
+        <source>Fatal error</source>
+        <translation>Критическая ошибка</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.h" line="58"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.h" line="61"/>
+        <source>
+
+Please send a bug report to the NomadBSD mailing list.
+
+We are sorry for the inconvenience
+</source>
+        <translation>
+
+Пожалуйста, отправьте отчет об ошибке в список рассылки NomadBSD.
+
+Извините за неудобства
+</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.h" line="65"/>
+        <location filename="../src/wizard.h" line="88"/>
+        <source>Shutdown</source>
+        <translation>Выключить</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.h" line="67"/>
+        <location filename="../src/wizard.h" line="90"/>
+        <source>Reboot</source>
+        <translation>Перезагрузить</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.h" line="69"/>
+        <location filename="../src/wizard.h" line="92"/>
+        <source>Exit setup</source>
+        <translation>Выйти из мастера настройки</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.h" line="84"/>
+        <source>Sure you want to cancel the setup?</source>
+        <translation>Уверены что хотите прервать настройку?</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.h" line="85"/>
+        <source>Cancel setup</source>
+        <translation>Прервать настройкy</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.h" line="95"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+</context>
+<context>
+    <name>SummaryPage</name>
+    <message>
+        <location filename="../src/wizard.cpp" line="692"/>
+        <source>Locale:</source>
+        <translation>Язык:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="693"/>
+        <source>Keyboard layout:</source>
+        <translation>Раскладка клавиатуры:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="694"/>
+        <source>Keyboard variant:</source>
+        <translation>Тип клавиатуры:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="695"/>
+        <source>Timezone:</source>
+        <translation>Временная зона:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="696"/>
+        <source>Encrypt /home:</source>
+        <translation>Зашифровать директорию /home:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="697"/>
+        <source>Shell:</source>
+        <translation>Командная оболочка:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="698"/>
+        <source>Editor:</source>
+        <translation>Текстовый редактор:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="699"/>
+        <source>GUI editor:</source>
+        <translation>Графический текстовый редактор:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="700"/>
+        <source>Email client:</source>
+        <translation>Почтовый клиент:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="701"/>
+        <source>Web browser:</source>
+        <translation>Web браузер:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="702"/>
+        <source>File manager:</source>
+        <translation>Файловый менеджер:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="709"/>
+        <source>
+
+If you click &quot;commit&quot;, the changes will be written to your USB flash drive</source>
+        <translation>
+
+Если Вы нажмете &quot;Применить&quot;, то изменения будут записаны на USB накопитель</translation>
+    </message>
+</context>
+<context>
+    <name>TimezonePage</name>
+    <message>
+        <location filename="../src/wizard.cpp" line="382"/>
+        <source>Couldn&apos;t start backend &apos;%1&apos;: %2</source>
+        <translation>Фоновый процесс не может быть запущен %1: %2</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="428"/>
+        <source>Please select your timezone</source>
+        <translation>Пожалуйста, выберите временную зону</translation>
+    </message>
+</context>
+<context>
+    <name>WelcomePage</name>
+    <message>
+        <location filename="../src/wizard.cpp" line="108"/>
+        <source>Welcome to the NomadBSD setup wizard.
+
+</source>
+        <translation>Добро пожаловать в мастер настройки NomadBSD.
+
+</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="109"/>
+        <source>Before you can use NomadBSD, there are some things that need to be set up.
+
+The setup will only write to the USB flash drive it is currently running from. It will not change your system.</source>
+        <translation>Нужно провести некоторую настройку прежде чем Вы сможете использовать NomadBSD.
+
+Мастер настройки будет записывать данные на USB накопитель с которого он запущен в данный момент. Ваша система не будет изменена.</translation>
+    </message>
+</context>
+</TS>
