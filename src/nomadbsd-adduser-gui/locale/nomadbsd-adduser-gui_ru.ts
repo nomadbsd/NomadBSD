cbc1d0360e1d6b824e5d73fb6369d94e382056d0
Marcel <mk@freeshell.de>
2019-11-16 11:39:14 +0100

    Add Russian translation files

---
+++ src/nomadbsd-adduser-gui/locale/nomadbsd-adduser-gui_ru.ts
@@ -0,0 +1,368 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>CommitPage</name>
+    <message>
+        <location filename="../src/wizard.cpp" line="748"/>
+        <source>Couldn&apos;t start backend &apos;%1&apos;: %2</source>
+        <translation>Фоновый процесс не может быть запущен &apos;%1&apos;: %2</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="772"/>
+        <source>An unexpected error occured while executing %1: %2</source>
+        <translation>Возникла неизвестная ошибка в процессе выполнения %1: %2</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="830"/>
+        <source>%1 returned with error code %2:
+&quot;%3&quot;</source>
+        <translation>%1 завершилась с кодом ошибки %2:
+&quot;%3&quot;</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="833"/>
+        <source>User %1 successfully added</source>
+        <translation>Пользователь %1 успешно добавлен</translation>
+    </message>
+</context>
+<context>
+    <name>ExtraKbdLayoutPage</name>
+    <message>
+        <location filename="../src/wizard.cpp" line="306"/>
+        <location filename="../src/wizard.cpp" line="337"/>
+        <source>Couldn&apos;t start backend &apos;%1&apos;: %2</source>
+        <translation>Фоновый процесс не может быть запущен &apos;%1&apos;: %2</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="326"/>
+        <location filename="../src/wizard.cpp" line="357"/>
+        <source>Command &apos;%1&apos; returned with an error.</source>
+        <translation>Команда &apos;%1&apos; завершилась с ошибкой.</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="481"/>
+        <source>Add layout</source>
+        <translation>Добавить раскладку</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="482"/>
+        <source>Remove layout</source>
+        <translation>Удалить раскладку</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="483"/>
+        <source>Additional keyboard layouts</source>
+        <translation>Дополнительные раскладки клавиатуры</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="484"/>
+        <source>Here you can add additional keyboard layouts. The user can switch between them from the NomadBSD desktop.</source>
+        <translation>Здесь Вы можете добавить дополнительные раскладки клавиатуры. Пользователь сможет переключаться между ними с рабочего стола NomadBSD.</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="487"/>
+        <source>Keyboard layout</source>
+        <translation>Раскладка клавиатуры</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="488"/>
+        <source>Keyboard variant</source>
+        <translation>Тип клавиатуры</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="489"/>
+        <source>Additional layouts</source>
+        <translation>Дополнительные раскладки</translation>
+    </message>
+</context>
+<context>
+    <name>LocalePage</name>
+    <message>
+        <location filename="../src/wizard.cpp" line="229"/>
+        <source>Couldn&apos;t start backend &apos;%1&apos;: %2</source>
+        <translation>Фоновый процесс не может быть запущен &apos;%1&apos;: %2</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="249"/>
+        <source>Command &apos;%1&apos; returned with an error.</source>
+        <translation>Команда &apos;%1&apos; завершилась с ошибкой.</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="253"/>
+        <source>Please select language/locale for the new user</source>
+        <translation>Пожалуйста, выберите язык/локализацию для нового пользователя</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordPage</name>
+    <message>
+        <location filename="../src/wizard.cpp" line="514"/>
+        <source>Set password for new user
+</source>
+        <translation>Установить пароль для нового пользователя
+</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="515"/>
+        <source>Password:</source>
+        <translation>Пароль:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="516"/>
+        <source>Repeat password:</source>
+        <translation>Повторите пароль:</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordWidget</name>
+    <message>
+        <location filename="../src/password.cpp" line="35"/>
+        <location filename="../src/password.cpp" line="97"/>
+        <source>Passwords do not match</source>
+        <translation>Пароли не совпадают</translation>
+    </message>
+    <message>
+        <location filename="../src/password.cpp" line="38"/>
+        <source>Password:</source>
+        <translation>Пароль:</translation>
+    </message>
+    <message>
+        <location filename="../src/password.cpp" line="39"/>
+        <source>Repeat password:</source>
+        <translation>Повторите пароль:</translation>
+    </message>
+    <message>
+        <location filename="../src/password.cpp" line="76"/>
+        <source>Only 7-bit ASCII characters are allowed</source>
+        <translation>Допустимы только 7-разрядные ASCII символы</translation>
+    </message>
+    <message>
+        <location filename="../src/password.cpp" line="93"/>
+        <source>OK</source>
+        <translation>ОК</translation>
+    </message>
+</context>
+<context>
+    <name>ProgramsPage</name>
+    <message>
+        <location filename="../src/wizard.cpp" line="576"/>
+        <source>Couldn&apos;t start backend &apos;%1&apos;: %2</source>
+        <translation>Фоновый процесс не может быть запущен &apos;%1&apos;: %2</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="598"/>
+        <source>Command &apos;%1&apos; returned with an error.</source>
+        <translation>Команда &apos;%1&apos; завершилась с ошибкой.</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="618"/>
+        <source>Shell</source>
+        <translation>Командная оболочка</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="618"/>
+        <source>Editor</source>
+        <translation>Текстовый редактор</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="618"/>
+        <source>GUI Editor</source>
+        <translation>Графический текстовый редактор</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="619"/>
+        <source>File manager</source>
+        <translation>Файловый менеджер</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="619"/>
+        <source>Mail client</source>
+        <translation>Почтовый клиент</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="619"/>
+        <source>Web browser</source>
+        <translation>Web браузер</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="623"/>
+        <source>Default applications
+</source>
+        <translation>Программы по-умолчанию
+</translation>
+    </message>
+</context>
+<context>
+    <name>SummaryPage</name>
+    <message>
+        <location filename="../src/wizard.cpp" line="674"/>
+        <source>Full name:</source>
+        <translation>Полное имя:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="675"/>
+        <source>Username:</source>
+        <translation>Имя пользователя:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="676"/>
+        <source>Locale:</source>
+        <translation>Локализация:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="677"/>
+        <source>Additional keyboard layouts:</source>
+        <translation>Дополнительные раскладки клавиатуры:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="678"/>
+        <source>Shell:</source>
+        <translation>Командная оболочка:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="679"/>
+        <source>Editor:</source>
+        <translation>Текстовый редактор:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="680"/>
+        <source>GUI editor:</source>
+        <translation>Графический текстовый редактор:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="681"/>
+        <source>File manager:</source>
+        <translation>Файловый менеджер:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="682"/>
+        <source>Mail client:</source>
+        <translation>Почтовый клиент:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="683"/>
+        <source>Web browser:</source>
+        <translation>Web браузер:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="689"/>
+        <source>Summary
+</source>
+        <translation>Конфигурация
+</translation>
+    </message>
+</context>
+<context>
+    <name>UsernamePage</name>
+    <message>
+        <location filename="../src/wizard.cpp" line="132"/>
+        <source>Full name:</source>
+        <translation>Полное имя:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="138"/>
+        <source>Username:</source>
+        <translation>Имя пользователя:</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="144"/>
+        <source>Add a new user to the system
+
+</source>
+        <translation>Добавить нового пользователя в систему
+
+</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="162"/>
+        <source>Username must not be empty</source>
+        <translation>Имя пользователя не должно быть пустым</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="167"/>
+        <source>Username already in use</source>
+        <translation>Имя пользователя уже используется</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="180"/>
+        <source>Couldn&apos;t read &apos;%1&apos;: %2</source>
+        <translation>Не могу прочесть &apos;%1&apos;: %2</translation>
+    </message>
+</context>
+<context>
+    <name>Wizard</name>
+    <message>
+        <location filename="../src/wizard.cpp" line="68"/>
+        <source>Add user</source>
+        <translation>Добавить пользователя</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="70"/>
+        <source>Next</source>
+        <translation>Далее</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="71"/>
+        <source>Back</source>
+        <translation>Назад</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="72"/>
+        <source>Commit</source>
+        <translation>Применить</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="73"/>
+        <source>Finish</source>
+        <translation>Завершить</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="74"/>
+        <location filename="../src/wizard.h" line="76"/>
+        <location filename="../src/wizard.h" line="82"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.h" line="58"/>
+        <source>Fatal error</source>
+        <translation>Критическая ошибка</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.h" line="59"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.h" line="62"/>
+        <source>
+
+Please send a bug report to the NomadBSD mailing list.
+
+We are sorry for the inconvenience
+</source>
+        <translation>
+
+Пожалуйста, отправьте сообщение об ошибке в список рассылки NomadBSD.
+
+Извините за неудобства
+</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.h" line="66"/>
+        <source>Exit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.h" line="75"/>
+        <source>Sure you want to quit?</source>
+        <translation>Вы уверены что хотите выйти?</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.h" line="79"/>
+        <source>Quit</source>
+        <translation>Выход</translation>
+    </message>
+</context>
+</TS>
---
+++ src/nomadbsd-dmconfig/locale/nomadbsd-dmconfig-gui_ru.ts
@@ -0,0 +1,138 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWin</name>
+    <message>
+        <location filename="../src/mainwin.cpp" line="40"/>
+        <source>Auto login</source>
+        <translation>Автоматический вход</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="41"/>
+        <source>Set default user</source>
+        <translation>Установить пользователя по-умолчанию</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="46"/>
+        <source>Display manager settings</source>
+        <translation>Настойки менеджера дисплея</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="54"/>
+        <source>Theme</source>
+        <translation>Тема</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="58"/>
+        <source>Preview</source>
+        <translation>Предпросмотр</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="67"/>
+        <source>Default user</source>
+        <translation>Пользователь по-умолчанию</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="82"/>
+        <location filename="../src/mainwin.cpp" line="132"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Сохранить</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="83"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Выйти</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="126"/>
+        <location filename="../src/mainwin.cpp" line="127"/>
+        <source>The file has been modified.</source>
+        <translation>Файл был изменён.</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="128"/>
+        <source>Do you want to save your changes?</source>
+        <translation>Хотите ли Вы сохранить изменения?</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="134"/>
+        <source>&amp;Quit without saving</source>
+        <translation>&amp;Выход без сохранения</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="135"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Отмена</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="221"/>
+        <location filename="../src/mainwin.cpp" line="283"/>
+        <source>Couldn&apos;t start backend &apos;%1&apos;: %2</source>
+        <translation>Фоновый процесс не может быть запущен &apos;%1&apos;: %2</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="230"/>
+        <source>Command &apos;%1&apos; returned with an error:
+%2</source>
+        <translation>Команда &apos;%1&apos; завершилась с ошибкой:
+%2</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="236"/>
+        <source>Saved</source>
+        <translation>Сохранено</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="315"/>
+        <source>Command &apos;%1&apos; returned with an error.</source>
+        <translation>Команда &apos;%1&apos; завершилась с ошибкой.</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.h" line="53"/>
+        <source>Fatal error</source>
+        <translation>Критическая ошибка</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.h" line="54"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.h" line="57"/>
+        <source>
+
+Please send a bug report to the NomadBSD mailing list.
+
+We are sorry for the inconvenience
+</source>
+        <translation>
+
+Пожалуйста, отправьте сообщение об ошибке в список рассылки NomadBSD.
+
+Извините за неудобства
+</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.h" line="61"/>
+        <source>Exit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.h" line="70"/>
+        <source>Sure you want to quit?</source>
+        <translation>Вы уверены что хотите выйти?</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.h" line="71"/>
+        <location filename="../src/mainwin.h" line="77"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.h" line="74"/>
+        <source>Quit</source>
+        <translation>Выход</translation>
+    </message>
+</context>
+</TS>
