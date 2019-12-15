dbdcd84e7f138c3b46a305442ee1e2456f1848dc
Grégory Reinbold <gregory@nosheep.fr>
2019-12-15 18:07:32 +0100

    Add French translations to nomadbsd-install-gui

--- src/nomadbsd-install-gui/locale/nomadbsd-install-gui_de.ts
+++ src/nomadbsd-install-gui/locale/nomadbsd-install-gui_de.ts
@@ -4,22 +4,22 @@
 <context>
     <name>CommitPage</name>
     <message>
-        <location filename="../src/wizard.cpp" line="409"/>
+        <location filename="../src/wizard.cpp" line="408"/>
         <source>Last chance to quit installation.</source>
         <translation>Letzte Chance, den Installationsvorgang abzubrechen.</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="410"/>
+        <location filename="../src/wizard.cpp" line="409"/>
         <source>Last chance</source>
         <translation>Letzte Chance</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="413"/>
+        <location filename="../src/wizard.cpp" line="412"/>
         <source>Quit</source>
         <translation>Beenden</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="416"/>
+        <location filename="../src/wizard.cpp" line="415"/>
         <source>Install</source>
         <translation>Installieren</translation>
     </message>
@@ -34,9 +34,8 @@
         <translation>Ein unerwarteter Fehler trat während der Ausführung von %1 auf: %2</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="515"/>
         <source>Press &quot;Finish&quot; to reboot</source>
-        <translation>Drücken Sie &quot;Abschließen&quot;, um den Computer neu zu starten</translation>
+        <translation type="vanished">Drücken Sie &quot;Abschließen&quot;, um den Computer neu zu starten</translation>
     </message>
 </context>
 <context>
@@ -116,72 +115,72 @@ Wir entschuldigen uns für die Unannehmlichkeiten
 <context>
     <name>SettingsPage</name>
     <message>
-        <location filename="../src/wizard.cpp" line="141"/>
+        <location filename="../src/wizard.cpp" line="140"/>
         <source>Couldn&apos;t start backend &apos;%1&apos;: %2</source>
         <translation>Konnte Backend &apos;%1&apos; nicht starten: %2</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="165"/>
+        <location filename="../src/wizard.cpp" line="164"/>
         <source>&lt;b&gt;Please select the device you want to install NomadBSD on&lt;/b&gt;</source>
         <translation>&lt;b&gt;Bitte wählen Sie das Gerät, auf das Sie NomadBSD installieren wollen&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="173"/>
+        <location filename="../src/wizard.cpp" line="172"/>
         <source>Target filesystem type</source>
         <translation>Zieldateisystem</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="174"/>
+        <location filename="../src/wizard.cpp" line="173"/>
         <source>&amp;UFS</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="175"/>
+        <location filename="../src/wizard.cpp" line="174"/>
         <source>&amp;ZFS</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="190"/>
+        <location filename="../src/wizard.cpp" line="189"/>
         <source>Enable lenovofix</source>
         <translation>Aktiviere lenovofix</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="191"/>
+        <location filename="../src/wizard.cpp" line="190"/>
         <source>Setting this allows Lenovo systems with a buggy BIOS to boot from GPT partitions</source>
         <translation>DIes ermöglicht Lenovo-Systemen mit fehlerhaftem BIOS von GPT-Partitionen zu booten</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="198"/>
+        <location filename="../src/wizard.cpp" line="197"/>
         <source>&lt;b&gt;Desired size of the swap partition&lt;/b&gt;</source>
         <translation>&lt;b&gt;Gewünschte Größe der Swap-Partition&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="211"/>
+        <location filename="../src/wizard.cpp" line="210"/>
         <source>&lt;b&gt;Username*&lt;/b&gt;</source>
         <translation>&lt;b&gt;Benutzername*&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="215"/>
+        <location filename="../src/wizard.cpp" line="214"/>
         <source>Auto-login user</source>
         <translation>Benutzer automatisch anmelden</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="217"/>
+        <location filename="../src/wizard.cpp" line="216"/>
         <source>&lt;i&gt;* The installation script will adopt nomad&apos;s complete account. Only the username changes.&lt;/i&gt;</source>
         <translation>&lt;i&gt;* Das Installations-Skript übernimmt nomads komplettes Benutzerkonto. Es ändert sich lediglich der Benutzername.&lt;/i&gt;</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="256"/>
+        <location filename="../src/wizard.cpp" line="255"/>
         <source>Username must not be empty</source>
         <translation>Benutzername darf nicht leer sein</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="261"/>
+        <location filename="../src/wizard.cpp" line="260"/>
         <source>Username already in use</source>
         <translation>Benutzername bereits vergeben</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="274"/>
+        <location filename="../src/wizard.cpp" line="273"/>
         <source>Couldn&apos;t read &apos;%1&apos;: %2</source>
         <translation>Konnte Datei &apos;%1&apos; nicht lesen: %2</translation>
     </message>
@@ -193,54 +192,54 @@ Wir entschuldigen uns für die Unannehmlichkeiten
 <context>
     <name>SummaryPage</name>
     <message>
-        <location filename="../src/wizard.cpp" line="342"/>
+        <location filename="../src/wizard.cpp" line="341"/>
         <source>&lt;b&gt;Summary:&lt;/b&gt;&lt;br/&gt;&lt;br/&gt;</source>
         <translation>&lt;b&gt;Zusammenfassung:&lt;/b&gt;&lt;br/&gt;&lt;br/&gt;</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="352"/>
-        <location filename="../src/wizard.cpp" line="354"/>
+        <location filename="../src/wizard.cpp" line="351"/>
+        <location filename="../src/wizard.cpp" line="353"/>
         <source>No</source>
         <translation>Nein</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="352"/>
-        <location filename="../src/wizard.cpp" line="354"/>
+        <location filename="../src/wizard.cpp" line="351"/>
+        <location filename="../src/wizard.cpp" line="353"/>
         <source>Yes</source>
         <translation>Ja</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="359"/>
+        <location filename="../src/wizard.cpp" line="358"/>
         <source>Target disk:</source>
         <translation>Ziellaufwerk:</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="360"/>
+        <location filename="../src/wizard.cpp" line="359"/>
         <source>Swap (in MB):</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="361"/>
+        <location filename="../src/wizard.cpp" line="360"/>
         <source>Username:</source>
         <translation>Benutzername:</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="362"/>
+        <location filename="../src/wizard.cpp" line="361"/>
         <source>Auto-login:</source>
         <translation>Automatisch anmelden:</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="363"/>
+        <location filename="../src/wizard.cpp" line="362"/>
         <source>Filesystem:</source>
         <translation>Dateisystem:</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="364"/>
+        <location filename="../src/wizard.cpp" line="363"/>
         <source>Lenovofix:</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="370"/>
+        <location filename="../src/wizard.cpp" line="369"/>
         <source>&lt;br/&gt;&lt;br/&gt;&lt;b&gt;Warning:&lt;br/&gt;&lt;br/&gt;&lt;/b&gt;
 If you click &lt;i&gt;commit&lt;/i&gt;, the system will be written to &lt;i&gt;%1&lt;/i&gt;. All data on &lt;i&gt;%1&lt;/i&gt; will be lost!</source>
         <translation>&lt;br/&gt;&lt;br/&gt;&lt;b&gt;Warnung:&lt;br/&gt;&lt;br/&gt;&lt;/b&gt;
@@ -250,12 +249,12 @@ Wenn Sie auf &lt;i&gt;Anwenden&lt;/i&gt; klicken, wird das System auf &lt;i&gt;%
 <context>
     <name>WelcomePage</name>
     <message>
-        <location filename="../src/wizard.cpp" line="97"/>
+        <location filename="../src/wizard.cpp" line="96"/>
         <source>&lt;b&gt;Welcome to the NomadBSD installation wizard.&lt;/b&gt;&lt;br/&gt;&lt;br/&gt;</source>
         <translation>&lt;b&gt;Willkommen zum NomadBSD Installations-Wizard&lt;/b&gt;&lt;br/&gt;&lt;br/&gt;</translation>
     </message>
     <message>
-        <location filename="../src/wizard.cpp" line="99"/>
+        <location filename="../src/wizard.cpp" line="98"/>
         <source>This program will install your NomadBSD with its current configuration on a hard disk.
 
 </source>
--- src/nomadbsd-install-gui/locale/nomadbsd-install-gui_fr.ts
+++ src/nomadbsd-install-gui/locale/nomadbsd-install-gui_fr.ts
@@ -1,41 +1,37 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="cs_CZ">
+<TS version="2.1" language="fr_FR">
 <context>
     <name>CommitPage</name>
     <message>
         <location filename="../src/wizard.cpp" line="408"/>
         <source>Last chance to quit installation.</source>
-        <translation>Poslední šance ukončit instalaci.</translation>
+        <translation>Dernière chance de quitter l&apos;installation.</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="409"/>
         <source>Last chance</source>
-        <translation>Poslední šance</translation>
+        <translation>Dernière chance</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="412"/>
         <source>Quit</source>
-        <translation>Ukončit</translation>
+        <translation>Quitter</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="415"/>
         <source>Install</source>
-        <translation>Instalovat</translation>
+        <translation>Installer</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="436"/>
         <source>Couldn&apos;t start backend &apos;%1&apos;: %2</source>
-        <translation>Není možné spustit %1: %2</translation>
+        <translation>Impossible de démarrer le processus &apos;%1&apos; : %2</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="453"/>
         <source>An unexpected error occured while executing %1: %2</source>
-        <translation>Nastala neočekávaná chyba při spouštění %1: %2</translation>
-    </message>
-    <message>
-        <source>Press &quot;Finish&quot; to reboot</source>
-        <translation type="vanished">Stiskni &quot;Dokončit&quot;, pro restartování</translation>
+        <translation>Une erreur s&apos;est produite lors de l&apos;exécution de %1 : %2</translation>
     </message>
 </context>
 <context>
@@ -43,37 +39,43 @@
     <message>
         <location filename="../src/wizard.cpp" line="60"/>
         <source>NomadBSD Installation Wizard</source>
-        <translation>Průvodce instalací NomadBSD</translation>
+        <translation>NomadBSD Assistant d&apos;Installation</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="63"/>
         <source>Next</source>
-        <translation>Další</translation>
+        <translation>Suivant</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="64"/>
         <source>Back</source>
-        <translation>Zpět</translation>
+        <translation>Précédent</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="65"/>
         <source>Commit</source>
-        <translation>Potvrdit</translation>
+        <translation>Appliquer</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="66"/>
         <source>Finish</source>
-        <translation>Dokončit</translation>
+        <translation>Terminer</translation>
+    </message>
+    <message>
+        <location filename="../src/wizard.cpp" line="67"/>
+        <location filename="../src/wizard.h" line="87"/>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
     </message>
     <message>
         <location filename="../src/wizard.h" line="64"/>
         <source>Fatal error</source>
-        <translation>Fatální chyba</translation>
+        <translation>Erreur fatale</translation>
     </message>
     <message>
         <location filename="../src/wizard.h" line="65"/>
         <source>Error</source>
-        <translation>Chyba</translation>
+        <translation>Erreur</translation>
     </message>
     <message>
         <location filename="../src/wizard.h" line="68"/>
@@ -85,31 +87,25 @@ We are sorry for the inconvenience
 </source>
         <translation>
 
-Prosím pošlete chybové hlášení na mailing list NomadBSD.
+Envoyez un rapport de bogue à la liste de diffusion de NomadBSD.
 
-Omlouváme se za potíže
+Nous sommes désolés pour le désagrément
 </translation>
     </message>
     <message>
         <location filename="../src/wizard.h" line="80"/>
         <source>Are you sure you want to quit?</source>
-        <translation>Jste si jistí, že chcete ukončit?</translation>
+        <translation>Êtes-vous sûr de vouloir quitter ?</translation>
     </message>
     <message>
         <location filename="../src/wizard.h" line="81"/>
         <source>Quit installation</source>
-        <translation>Ukončit instalaci</translation>
+        <translation>Quitter l&apos;installation</translation>
     </message>
     <message>
         <location filename="../src/wizard.h" line="84"/>
         <source>Quit</source>
-        <translation>Ukončit</translation>
-    </message>
-    <message>
-        <location filename="../src/wizard.cpp" line="67"/>
-        <location filename="../src/wizard.h" line="87"/>
-        <source>Cancel</source>
-        <translation>Zrušit</translation>
+        <translation>Quitter</translation>
     </message>
 </context>
 <context>
@@ -117,76 +113,72 @@ Omlouváme se za potíže
     <message>
         <location filename="../src/wizard.cpp" line="140"/>
         <source>Couldn&apos;t start backend &apos;%1&apos;: %2</source>
-        <translation>Není možné spustit %1: %2</translation>
+        <translation>Impossible de démarrer le processus &apos;%1&apos; : %2</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="164"/>
         <source>&lt;b&gt;Please select the device you want to install NomadBSD on&lt;/b&gt;</source>
-        <translation>&lt;b&gt;Prosím vyberte zařízení na které chcete NomadBSD nainstalovat&lt;/b&gt;</translation>
+        <translation>&lt;b&gt;Sélectionnez le disque sur lequel vous souhaitez installer NomadBSD&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="172"/>
         <source>Target filesystem type</source>
-        <translation>Cílový typ souborového systému</translation>
+        <translation>Type de système de fichiers cible</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="173"/>
         <source>&amp;UFS</source>
-        <translation></translation>
+        <translation>&amp;UFS</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="174"/>
         <source>&amp;ZFS</source>
-        <translation></translation>
+        <translation>&amp;ZFS</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="189"/>
         <source>Enable lenovofix</source>
-        <translation>Aktivovat lenovofix</translation>
+        <translation>Activer lenovofix</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="190"/>
         <source>Setting this allows Lenovo systems with a buggy BIOS to boot from GPT partitions</source>
-        <translation>Toto nastavení umožní Lenovu s chybným BIOSem zavést systém z GPT oddílu</translation>
+        <translation>Activer ceci permet aux systèmes Lenovo avec un BIOS bogué de démarrer depuis une partition GPT</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="197"/>
         <source>&lt;b&gt;Desired size of the swap partition&lt;/b&gt;</source>
-        <translation>&lt;b&gt;Velikost odkládacího oddílu Swap&lt;/b&gt;</translation>
+        <translation>&lt;b&gt;Taille de la partition swap&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="210"/>
         <source>&lt;b&gt;Username*&lt;/b&gt;</source>
-        <translation>&lt;b&gt;Uživatelské jméno*&lt;/b&gt;</translation>
+        <translation>&lt;b&gt;Nom d&apos;utilisateur&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="214"/>
         <source>Auto-login user</source>
-        <translation>Automaticky přihlašovat uživatele</translation>
+        <translation>S&apos;identifier automatiquement avec cet utilisateur</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="216"/>
         <source>&lt;i&gt;* The installation script will adopt nomad&apos;s complete account. Only the username changes.&lt;/i&gt;</source>
-        <translation>&lt;i&gt;* Instalační skript použije účet nomad. Pouze uživatelské jméno se změní.&lt;/i&gt;</translation>
+        <translation>&lt;i&gt;* Le script d&apos;installation adoptera complètement le compte nomad. Seulement le nom d&apos;utilisateur change.&lt;/i&gt;</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="255"/>
         <source>Username must not be empty</source>
-        <translation>Uživatelské jméno nesmí být prázdné</translation>
+        <translation>Le nom d&apos;utilisateur ne peut être vide</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="260"/>
         <source>Username already in use</source>
-        <translation>Uživatelské jméno již někdo používá</translation>
+        <translation>Nom d&apos;utilisateur déjà utilisé</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="273"/>
         <source>Couldn&apos;t read &apos;%1&apos;: %2</source>
-        <translation>Nelze přečíst %1: %2</translation>
-    </message>
-    <message>
-        <source>Username must be at least 2 characters long</source>
-        <translation type="vanished">Uživatelské jméno musím mít aspoň 2 znaky</translation>
+        <translation>Impossible de lire &apos;%1&apos; : %2</translation>
     </message>
 </context>
 <context>
@@ -194,56 +186,56 @@ Omlouváme se za potíže
     <message>
         <location filename="../src/wizard.cpp" line="341"/>
         <source>&lt;b&gt;Summary:&lt;/b&gt;&lt;br/&gt;&lt;br/&gt;</source>
-        <translation>&lt;b&gt;Souhrn:&lt;/b&gt;&lt;br/&gt;&lt;br/&gt;</translation>
+        <translation>&lt;b&gt;Résumé :&lt;/b&gt;&lt;br/&gt;&lt;br/&gt;</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="351"/>
         <location filename="../src/wizard.cpp" line="353"/>
         <source>No</source>
-        <translation>Ne</translation>
+        <translation>Non</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="351"/>
         <location filename="../src/wizard.cpp" line="353"/>
         <source>Yes</source>
-        <translation>Ano</translation>
+        <translation>Oui</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="358"/>
         <source>Target disk:</source>
-        <translation>Cílový disk:</translation>
+        <translation>Disque cible :</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="359"/>
         <source>Swap (in MB):</source>
-        <translation>Swap (v MB):</translation>
+        <translation>Swap (en MB) :</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="360"/>
         <source>Username:</source>
-        <translation>Uživatelské jméno:</translation>
+        <translation>Nom d&apos;utilisateur :</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="361"/>
         <source>Auto-login:</source>
-        <translation>Automatické přihlášení::</translation>
+        <translation>Identification automatique :</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="362"/>
         <source>Filesystem:</source>
-        <translation>Souborový systém:</translation>
+        <translation>Système de fichiers :</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="363"/>
         <source>Lenovofix:</source>
-        <translation>Lenovofix:</translation>
+        <translation>Lenovofix :</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="369"/>
         <source>&lt;br/&gt;&lt;br/&gt;&lt;b&gt;Warning:&lt;br/&gt;&lt;br/&gt;&lt;/b&gt;
 If you click &lt;i&gt;commit&lt;/i&gt;, the system will be written to &lt;i&gt;%1&lt;/i&gt;. All data on &lt;i&gt;%1&lt;/i&gt; will be lost!</source>
-        <translation>&lt;br/&gt;&lt;br/&gt;&lt;b&gt;Varování:&lt;br/&gt;&lt;br/&gt;&lt;/b&gt;
-Jestli zmáčknete &lt;i&gt;Potvrdit&lt;/i&gt; systém bude zapsán na &lt;i&gt;%1&lt;/i&gt;. Veškerá data na &lt;i&gt;%1&lt;/i&gt; budou ztracena!</translation>
+        <translation>&lt;br/&gt;&lt;br/&gt;&lt;b&gt;Avertissement :&lt;br/&gt;&lt;br/&gt;&lt;/b&gt;
+Si vous cliquez sur &lt;i&gt;appliquer&lt;/i&gt;, le système sera écrit sur &lt;i&gt;%1&lt;/i&gt;. Toutes les données sur &lt;i&gt;%1&lt;/i&gt; seront perdues !</translation>
     </message>
 </context>
 <context>
@@ -251,14 +243,14 @@ Jestli zmáčknete &lt;i&gt;Potvrdit&lt;/i&gt; systém bude zapsán na &lt;i&gt;
     <message>
         <location filename="../src/wizard.cpp" line="96"/>
         <source>&lt;b&gt;Welcome to the NomadBSD installation wizard.&lt;/b&gt;&lt;br/&gt;&lt;br/&gt;</source>
-        <translation>&lt;b&gt;Vítejte v průvodci instlací NomadBSD.&lt;/b&gt;&lt;br/&gt;&lt;br/&gt;</translation>
+        <translation>&lt;b&gt;Bienvenue dans l&apos;assistant d&apos;installation de NomadBSD.&lt;/b&gt;&lt;br/&gt;&lt;br/&gt;</translation>
     </message>
     <message>
         <location filename="../src/wizard.cpp" line="98"/>
         <source>This program will install your NomadBSD with its current configuration on a hard disk.
 
 </source>
-        <translation>Tento program nainstaluje NomadBSD se současnou konfigurací systému na pevný disk.
+        <translation>Ce programme va installer votre NomadBSD avec sa configuration actuelle sur un disque dur.
 
 </translation>
     </message>
