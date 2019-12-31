0f5c20c2071d0678dcf3bf9e371f5f621f46f36e
kazeabbey <bayu_luke@yahoo.com>
2019-12-31 15:54:13 +0700

    Add Indonesian translation file.

---
+++ src/nomadbsd-dmconfig/locale/nomadbsd-dmconfig-gui_id.ts
@@ -0,0 +1,137 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="id_ID">
+<context>
+    <name>MainWin</name>
+    <message>
+        <location filename="../src/mainwin.cpp" line="40"/>
+        <source>Auto login</source>
+        <translation>Masuk-otomatis</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="41"/>
+        <source>Set default user</source>
+        <translation>Buat pengguna standar</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="46"/>
+        <source>Display manager settings</source>
+        <translation>Pengaturan manajer tampilan</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="54"/>
+        <source>Theme</source>
+        <translation>Tema</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="58"/>
+        <source>Preview</source>
+        <translation>Pratinjau</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="67"/>
+        <source>Default user</source>
+        <translation>Pengguna standar</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="82"/>
+        <location filename="../src/mainwin.cpp" line="132"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Simpan</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="83"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Keluar</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="126"/>
+        <location filename="../src/mainwin.cpp" line="127"/>
+        <source>The file has been modified.</source>
+        <translation>Berkas sudah dimodifikasi.</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="128"/>
+        <source>Do you want to save your changes?</source>
+        <translation>Anda ingin menyimpan perubahan?</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="134"/>
+        <source>&amp;Quit without saving</source>
+        <translation>&amp;Keluar tanpa menyimpan</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="135"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Batal</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="221"/>
+        <location filename="../src/mainwin.cpp" line="283"/>
+        <source>Couldn&apos;t start backend &apos;%1&apos;: %2</source>
+        <translation>Tidak dapat memulai layanan &apos;%1&apos;: %2</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="230"/>
+        <source>Command &apos;%1&apos; returned with an error:
+%2</source>
+        <translation>Perintah &apos;%1&apos;kembali dengan kesalahan:
+%2</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="236"/>
+        <source>Saved</source>
+        <translation>Disimpan</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="315"/>
+        <source>Command &apos;%1&apos; returned with an error.</source>
+        <translation>Perintah &apos;%1&apos; kembali dengan kesalahan.</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.h" line="53"/>
+        <source>Fatal error</source>
+        <translation>Kesalahan fatal</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.h" line="54"/>
+        <source>Error</source>
+        <translation>Kesalahan</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.h" line="57"/>
+        <source>
+
+Please send a bug report to the NomadBSD mailing list.
+
+We are sorry for the inconvenience
+</source>
+        <translation>
+
+Silahkan kirim laporan kutu ke milis NomadBSD.
+
+Mohon maaf atas ketidaknyamanan ini</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.h" line="61"/>
+        <source>Exit</source>
+        <translation>Keluar</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.h" line="70"/>
+        <source>Sure you want to quit?</source>
+        <translation>Anda yakin ingin keluar?</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.h" line="71"/>
+        <location filename="../src/mainwin.h" line="77"/>
+        <source>Cancel</source>
+        <translation>Batal</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.h" line="74"/>
+        <source>Quit</source>
+        <translation>Keluar</translation>
+    </message>
+</context>
+</TS>
