ab7f5a3673cbe36e5912bdf1addc3f9cee262145
Martin Orszulik <orszulik.m@outlook.com>
2020-01-16 16:09:26 +0100

    Add a Czech translation file for nomadbsd-chusr

--- src/nomadbsd-chusr/locale/nomadbsd-chusr_de.ts
+++ src/nomadbsd-chusr/locale/nomadbsd-chusr_de.ts
@@ -6,57 +6,57 @@
     <message>
         <location filename="../nomadbsd-chusr" line="431"/>
         <source>&amp;Quit</source>
-        <translation>&amp;Beenden</translation>
+        <translation>&amp;Ukončit</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="431"/>
         <source>&amp;Save</source>
-        <translation>&amp;Speichern</translation>
+        <translation>&amp;Uložit</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="83"/>
         <source>General settings</source>
-        <translation>Allgemeine Einstellungen</translation>
+        <translation>Hlavní nastavení</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="167"/>
         <source>Name:</source>
-        <translation>Name:</translation>
+        <translation>Jméno:</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="400"/>
         <source>Saved</source>
-        <translation>Gespeichert</translation>
+        <translation>Uloženo</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="162"/>
         <source>User:</source>
-        <translation>Benutzer:</translation>
+        <translation>Uživatel:</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="125"/>
         <source>Change password</source>
-        <translation>Ändere Passwort</translation>
+        <translation>Změnit heslo</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="123"/>
         <source>Repeat password:</source>
-        <translation>Passwort wiederholen:</translation>
+        <translation>Heslo znovu:</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="153"/>
         <source>Password successfully changed</source>
-        <translation>Passwort erfolgreich geändert</translation>
+        <translation>Heslo bylo úspěšně změněno</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="178"/>
         <source>Login class:</source>
-        <translation>Login-Klasse:</translation>
+        <translation>Typ přihlášení:</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="170"/>
         <source>Login group:</source>
-        <translation>Login-Gruppe:</translation>
+        <translation>Skupina přihlášení:</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="181"/>
@@ -66,107 +66,107 @@
     <message>
         <location filename="../nomadbsd-chusr" line="443"/>
         <source>Modified</source>
-        <translation>Geändert</translation>
+        <translation>Změněno</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="420"/>
         <source>pw returned with an error</source>
-        <translation>pw mit Fehler beendet</translation>
+        <translation>pw se vrátil s chybou</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="207"/>
         <source>Discard changes?</source>
-        <translation>Änderungen verwerfen?</translation>
+        <translation>Zahodit změny?</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="84"/>
         <source>Password</source>
-        <translation>Passwort</translation>
+        <translation>Heslo</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="139"/>
         <source>Passwords do not match</source>
-        <translation>Passwörter stimmen nicht überein</translation>
+        <translation>Hesla se neshodují</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="175"/>
         <source>Secondary groups:</source>
-        <translation>Sekundäre Gruppen:</translation>
+        <translation>Sekundární skupiny:</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="431"/>
         <source>&amp;Cancel</source>
-        <translation>&amp;Abbrechen</translation>
+        <translation>&amp;Zrušit</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="122"/>
         <source>Password:</source>
-        <translation>Passwort:</translation>
+        <translation>Helso:</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="207"/>
         <source>&amp;Discard</source>
-        <translation>&amp;Verwerfen</translation>
+        <translation>&amp;Zahodit</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="87"/>
         <source>&amp;Delete account</source>
-        <translation>&amp;Lösche Konto</translation>
+        <translation>&amp;Zrušit účet</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="381"/>
         <source>Unknown group</source>
-        <translation>Unbekannte Gruppe</translation>
+        <translation>Neznámá skupina</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="207"/>
         <source>Changes have not been saved. Discard?</source>
-        <translation>Änderungen wurden nicht gespeichert. Verwerfen?</translation>
+        <translation>Změny nebyly uloženy. Zahodit?</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="381"/>
         <source>Unknown group &quot;{0}&quot;</source>
-        <translation>Unbekannte Gruppe &quot;{0}&quot;</translation>
+        <translation>Neznámá skupina &quot;{0}&quot;</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="141"/>
         <source>Password must not be empty</source>
-        <translation>Passwort darf nicht leer sein</translation>
+        <translation>Heslo nesmí být prázdné</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="431"/>
         <source>Really quit?</source>
-        <translation>Wirklich beenden?</translation>
+        <translation>Opravdu ukončit?</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="431"/>
         <source>Quit without saving?</source>
-        <translation>Beenden ohne zu speichern?</translation>
+        <translation>Ukončit bez uložení?</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="404"/>
         <source>Sure to delete account?</source>
-        <translation>Konto wirklich löschen?</translation>
+        <translation>Určitě chcete smazat účet?</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="404"/>
         <source>Are you sure you want to delete the account for user {0}?</source>
-        <translation>Sind Sie sicher, dass Sie das Konto des Benutzers {0} löschen wollen?</translation>
+        <translation>Jste si jistí, že chcete smazat účet pro uživatele {0} ?</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="404"/>
         <source>Delete account</source>
-        <translation>Konto löschen</translation>
+        <translation>Smazat účet</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="404"/>
         <source>Cancel</source>
-        <translation>Abbrechen</translation>
+        <translation>Zrušit</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="423"/>
         <source>Account deleted</source>
-        <translation>Konto gelöscht</translation>
+        <translation>Účet byl smazán</translation>
     </message>
 </context>
 <context>
@@ -174,12 +174,12 @@
     <message>
         <location filename="../nomadbsd-chusr" line="477"/>
         <source>Permission denied</source>
-        <translation>Fehlende Berechtigung</translation>
+        <translation>Přístup odmítnut</translation>
     </message>
     <message>
         <location filename="../nomadbsd-chusr" line="473"/>
         <source>Error: You must be root to run this program</source>
-        <translation>Fehler: Sie müssen root sein, um dieses Programm auszuführen</translation>
+        <translation>Chyba: musíte být root pro spuštění tohot programu</translation>
     </message>
 </context>
 <context>
@@ -187,7 +187,7 @@
     <message>
         <location filename="../nomadbsd-chusr" line="475"/>
         <source>You must be root to run this program</source>
-        <translation>Sie müssen root sein, um dieses Programm auszuführen</translation>
+        <translation>Musíte být root pro spuštění tohot programu</translation>
     </message>
 </context>
 </TS>
