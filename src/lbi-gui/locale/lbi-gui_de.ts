cffd11b099ed95bd13cb59958fb127cc72300308
Marcel <mk@freeshell.de>
2021-01-01 15:58:45 +0100

    Update

--- src/lbi-gui/locale/lbi-gui_de.ts
+++ src/lbi-gui/locale/lbi-gui_de.ts
@@ -4,89 +4,99 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../lbi-gui" line="46"/>
+        <location filename="../lbi-gui" line="51"/>
         <source>&amp;Install</source>
         <translation>&amp;Installieren</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="47"/>
+        <location filename="../lbi-gui" line="52"/>
         <source>&amp;Deinstall</source>
         <translation>&amp;Deinstallieren</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="49"/>
+        <location filename="../lbi-gui" line="54"/>
         <source>&amp;Quit</source>
         <translation>&amp;Beenden</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="54"/>
+        <location filename="../lbi-gui" line="59"/>
         <source>This program installs the &lt;em&gt;Widevine&lt;/em&gt; capable Linux version of the &lt;em&gt;Google Chrome&lt;/em&gt; or &lt;em&gt;Brave&lt;/em&gt; browser. Both browsers allow you to use web services like &lt;em&gt;Netflix&lt;/em&gt;, &lt;em&gt;Prime Video&lt;/em&gt;, or &lt;em&gt;Spotify&lt;/em&gt;
 
 </source>
-        <translation>Dieses Programm installiert die &lt;em&gt;Widevine&lt;/em&gt;-fähige Linux-Version des &lt;em&gt;Google Chrome&lt;/em&gt;- oder &lt;em&gt;Brave&lt;/em&gt;-Browsers. Beide Browser ermöglichen es, &lt;em&gt;Netflix&lt;/em&gt;, &lt;em&gt;Prime Video&lt;/em&gt; oder &lt;em&gt;Spotify&lt;/em&gt; zu benutzen.
+        <translation>Dieses Programm installiert die &lt;em&gt;Widevine&lt;/em&gt;-fähige Linux-Version des &lt;em&gt;Google Chrome&lt;/em&gt;- oder &lt;em&gt;Brave&lt;/em&gt;-Browsers. Beide Browser ermöglichen es, &lt;em&gt;Netflix&lt;/em&gt;, &lt;em&gt;Prime Video&lt;/em&gt; oder &lt;em&gt;Spotify&lt;/em&gt; zu benutzen
 
 </translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="63"/>
+        <location filename="../lbi-gui" line="68"/>
         <source>Linux Browser Installer GUI</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="196"/>
+        <location filename="../lbi-gui" line="212"/>
         <source>Error</source>
         <translation>Fehler</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="186"/>
+        <location filename="../lbi-gui" line="202"/>
         <source>Command &quot;{}&quot; exited with error code {}</source>
         <translation>Befehle &quot;{}&quot; mit Fehlercode {} beendet</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="196"/>
+        <location filename="../lbi-gui" line="212"/>
         <source>Command {} exited with error code {}</source>
         <translation>Befehl &quot;{}&quot; mit Fehlercode {} beendet</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="48"/>
+        <location filename="../lbi-gui" line="53"/>
         <source>&amp;Cancel</source>
         <translation>&amp;Abbrechen</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="137"/>
+        <location filename="../lbi-gui" line="120"/>
         <source>Cloning repo ...</source>
         <translation>Klone Repo ...</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="123"/>
+        <location filename="../lbi-gui" line="135"/>
         <source>Installing ...</source>
         <translation>Installiere ...</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="146"/>
+        <location filename="../lbi-gui" line="150"/>
         <source>Deinstalling ...</source>
         <translation>Deinstalliere ...</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="191"/>
+        <location filename="../lbi-gui" line="207"/>
         <source>Done</source>
         <translation>Fertig</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="208"/>
+        <location filename="../lbi-gui" line="224"/>
         <source>Canceled</source>
         <translation>Abgebrochen</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui" line="57"/>
+        <source>&amp;Upgrade browser(s)</source>
+        <translation>Alle Browser &amp;aktualisieren</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui" line="161"/>
+        <source>Upgrading ...</source>
+        <translation>Aktualisiere ...</translation>
     </message>
 </context>
 <context>
     <name>win</name>
     <message>
-        <location filename="../lbi-gui" line="230"/>
+        <location filename="../lbi-gui" line="251"/>
         <source>Error</source>
         <translation>Fehler</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="230"/>
+        <location filename="../lbi-gui" line="251"/>
         <source>You must be root to run this program</source>
         <translation>Sie müssen dieses Programm als root ausführen</translation>
     </message>
