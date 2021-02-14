fe9de372b987e7d5988c795424f39025cc4b7794
Marcel <mk@freeshell.de>
2021-02-14 12:09:09 +0100

    Update

--- src/lbi-gui/locale/lbi-gui_de.ts
+++ src/lbi-gui/locale/lbi-gui_de.ts
@@ -4,86 +4,111 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../lbi-gui" line="51"/>
+        <location filename="../lbi-gui" line="52"/>
         <source>&amp;Install</source>
         <translation>&amp;Installieren</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="52"/>
+        <location filename="../lbi-gui" line="53"/>
         <source>&amp;Deinstall</source>
         <translation>&amp;Deinstallieren</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="54"/>
+        <location filename="../lbi-gui" line="55"/>
         <source>&amp;Quit</source>
         <translation>&amp;Beenden</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="70"/>
+        <location filename="../lbi-gui" line="71"/>
         <source>Linux Browser Installer GUI</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="218"/>
+        <location filename="../lbi-gui" line="257"/>
         <source>Error</source>
         <translation>Fehler</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="208"/>
+        <location filename="../lbi-gui" line="238"/>
         <source>Command &quot;{}&quot; exited with error code {}</source>
         <translation>Befehle &quot;{}&quot; mit Fehlercode {} beendet</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="218"/>
+        <location filename="../lbi-gui" line="257"/>
         <source>Command {} exited with error code {}</source>
         <translation>Befehl &quot;{}&quot; mit Fehlercode {} beendet</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="53"/>
+        <location filename="../lbi-gui" line="54"/>
         <source>&amp;Cancel</source>
         <translation>&amp;Abbrechen</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="121"/>
+        <location filename="../lbi-gui" line="134"/>
         <source>Cloning repo ...</source>
         <translation>Klone Repo ...</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="136"/>
+        <location filename="../lbi-gui" line="154"/>
         <source>Installing ...</source>
         <translation>Installiere ...</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="151"/>
+        <location filename="../lbi-gui" line="174"/>
         <source>Deinstalling ...</source>
         <translation>Deinstalliere ...</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="213"/>
+        <location filename="../lbi-gui" line="243"/>
         <source>Done</source>
         <translation>Fertig</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="230"/>
+        <location filename="../lbi-gui" line="269"/>
         <source>Canceled</source>
         <translation>Abgebrochen</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="57"/>
+        <location filename="../lbi-gui" line="58"/>
         <source>&amp;Upgrade browser(s)</source>
         <translation>Alle Browser &amp;aktualisieren</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="162"/>
+        <location filename="../lbi-gui" line="185"/>
         <source>Upgrading ...</source>
         <translation>Aktualisiere ...</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="59"/>
-        <source>This program installs the &lt;em&gt;Widevine&lt;/em&gt; capable Linux version of the &lt;em&gt;Google Chrome&lt;/em&gt; or &lt;em&gt;Brave&lt;/em&gt; browser. Both browsers allow you to use web services like &lt;em&gt;Netflix&lt;/em&gt;, &lt;em&gt;Prime Video&lt;/em&gt;, or &lt;em&gt;Spotify&lt;/em&gt;. The installation requires approx. 1GB for the Linux environment, and approx. 100MB for the browser.
+        <location filename="../lbi-gui" line="245"/>
+        <source>Installation of {self.browser} finished</source>
+        <translation>Installation von {self.browser} abgeschlossen</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui" line="247"/>
+        <source>Deinstallation of {self.browser} finished</source>
+        <translation>Deinstallation von {self.browser} abgeschlossen</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui" line="249"/>
+        <source>Upgrade finished</source>
+        <translation>Upgrade abgeschlossen</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui" line="251"/>
+        <source>Command finished</source>
+        <translation>Befehl abgeschlossen</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui" line="252"/>
+        <source>Finished</source>
+        <translation>Fertig</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui" line="60"/>
+        <source>This program installs the &lt;em&gt;Widevine&lt;/em&gt; capable Linux version of &lt;em&gt;Google Chrome&lt;/em&gt;, the &lt;em&gt;Brave&lt;/em&gt; or &lt;em&gt;Vivaldi&lt;/em&gt; browser. They allow you to use web services like &lt;em&gt;Netflix&lt;/em&gt;, &lt;em&gt;Prime Video&lt;/em&gt;, or &lt;em&gt;Spotify&lt;/em&gt;. The installation requires approx. 1GB for the Linux environment, and approx. 100MB for the browser.
 
 </source>
-        <translation>Dieses Programm installiert die &lt;em&gt;Widevine&lt;/em&gt;-fähige Linux-Version des &lt;em&gt;Google Chrome&lt;/em&gt;- oder &lt;em&gt;Brave&lt;/em&gt;-Browsers. Beide Browser ermöglichen es, &lt;em&gt;Netflix&lt;/em&gt;, &lt;em&gt;Prime Video&lt;/em&gt; oder &lt;em&gt;Spotify&lt;/em&gt; zu benutzen. Die Installation benötigt etwa 1GB für die Linux-Umgebung und ca. 100MB für den Browser.
+        <translation>Dieses Programm installiert die &lt;em&gt;Widevine&lt;/em&gt;-fähige Linux-Version von &lt;em&gt;Google Chrome&lt;/em&gt;, dem &lt;em&gt;Brave&lt;/em&gt;- oder &lt;em&gt;Vivaldi&lt;/em&gt;-Browser. Sie ermöglichen die Nutzung von z.B. &lt;em&gt;Netflix&lt;/em&gt;, &lt;em&gt;Prime Video&lt;/em&gt; oder &lt;em&gt;Spotify&lt;/em&gt;. Die Installtion benötigt etwa 1GB für die Linux-Umgebung, und ungefähr 100MB für den jeweiligen Browser.
 
 </translation>
     </message>
@@ -91,12 +116,12 @@
 <context>
     <name>win</name>
     <message>
-        <location filename="../lbi-gui" line="251"/>
+        <location filename="../lbi-gui" line="291"/>
         <source>Error</source>
         <translation>Fehler</translation>
     </message>
     <message>
-        <location filename="../lbi-gui" line="251"/>
+        <location filename="../lbi-gui" line="291"/>
         <source>You must be root to run this program</source>
         <translation>Sie müssen dieses Programm als root ausführen</translation>
     </message>
