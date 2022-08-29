7f31c2d45d92b6837a741c8eeec9c3df87500c1d
Mert Gunduz <merttgg@gmail.com>
2022-08-29 17:28:41 +0300

    Add Turkish Translations lbi-gui.pro

---
+++ src/lbi-gui/locale/lb-gui_tr.ts
@@ -0,0 +1,129 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../lbi-gui.in" line="53"/>
+        <source>&amp;Install</source>
+        <translation>&amp;İndir</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="54"/>
+        <source>&amp;Deinstall</source>
+        <translation>&amp;Kaldır</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="56"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Çıkış</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="70"/>
+        <source>Linux Browser Installer GUI</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="271"/>
+        <source>Error</source>
+        <translation>Hata</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="252"/>
+        <source>Command &quot;{}&quot; exited with error code {}</source>
+        <translation>Befehle &amp;quot;{}&amp;quot; Hata kodu {} ile çıkıldı</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="271"/>
+        <source>Command {} exited with error code {}</source>
+        <translation>Befehl &amp;quot;{}&amp;quot; Hata kodu ile {} çıkıldı</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="55"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;İptal</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="135"/>
+        <source>Cloning repo ...</source>
+        <translation>Repo klonlanıyor...</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="158"/>
+        <source>Installing ...</source>
+        <translation>Yükleniyor...</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="181"/>
+        <source>Deinstalling ...</source>
+        <translation>Kaldırılıyor...</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="257"/>
+        <source>Done</source>
+        <translation>Tamamlandı</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="283"/>
+        <source>Canceled</source>
+        <translation>İptal edildi</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="59"/>
+        <source>&amp;Upgrade browser(s)</source>
+        <translation>&amp;Tarayıcıları güncelle</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="192"/>
+        <source>Upgrading ...</source>
+        <translation>Güncelleniyor...</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="263"/>
+        <source>Upgrade finished</source>
+        <translation>Güncelleme tamamlandı</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="265"/>
+        <source>Command finished</source>
+        <translation>Komut tamamlandı</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="266"/>
+        <source>Finished</source>
+        <translation>Tamamlandı</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="259"/>
+        <source>Installation of {} finished</source>
+        <translation>{} indirmesi tamamlandı</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="261"/>
+        <source>Deinstallation of {} finished</source>
+        <translation>{} kaldırması tamamlandı</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="61"/>
+        <source>This program installs Linux binaries of &lt;em&gt;Widevine&lt;/em&gt; capable browsers. They allow you to use web services like &lt;em&gt;Netflix&lt;/em&gt;, &lt;em&gt;Prime Video&lt;/em&gt;, or &lt;em&gt;Spotify&lt;/em&gt;. The installation requires approx. 1GB for the Linux environment, and approx. 100MB for the browser.
+
+</source>
+        <translation>Bu program &lt;em&gt;Widevine&lt;\'in Linux binarylerini indirir/em&gt; Kullanıcı için uygun tarayıcılardır, &lt;em&gt;Netflix&lt;/em&gt;, &lt;em&gt;Prime Video&lt;/em&gt; ve &lt;em&gt;Spotify&lt;/em&gt gibi web servislerini kullanmanızı sağlar.;. İndirme Linux ortamı için yaklaşık olarak 1GB ve her tarayıcı için yaklaşık olarak 100 MB'dır.
+
+</translation>
+    </message>
+</context>
+<context>
+    <name>win</name>
+    <message>
+        <location filename="../lbi-gui.in" line="306"/>
+        <source>Error</source>
+        <translation>Hata</translation>
+    </message>
+    <message>
+        <location filename="../lbi-gui.in" line="306"/>
+        <source>You must be root to run this program</source>
+        <translation>Bu programı çalıştırmak için yönetici olmalısınız</translation>
+    </message>
+</context>
+</TS>
