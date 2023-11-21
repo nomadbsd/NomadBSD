b6330db516be32533862baf9c60d99a3b71342e0
Marcel <mk@freeshell.de>
2023-11-21 16:25:20 +0100

    Update

---
+++ nomad/.mozilla/firefox/g6qyxmpo.default-esr/user.js
@@ -0,0 +1,7 @@
+user_pref("browser.sessionstore.interval", 1800000);
+user_pref("browser.cache.disk.enable", false);
+user_pref("browser.cache.memory.enable", true);
+user_pref("browser.cache.memory.capacity", -1);
+user_pref("extensions.pocket.enabled", false);
+user_pref("browser.shell.checkDefaultBrowser", false);
+
